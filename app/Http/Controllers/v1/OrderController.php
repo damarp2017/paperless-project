c15ac6791ef774298efc3e18c3232da46fb09f29
Damar Permadany <damarp2017@gmail.com>
2020-04-29 06:19:50 +0700

    check bug

--- app/Http/Controllers/v1/OrderController.php
+++ app/Http/Controllers/v1/OrderController.php
@@ -12,33 +12,41 @@ class OrderController extends Controller
 {
     public function store(Request $request)
     {
-        $data = $request->all();
+        try {
+            $data = $request->all();
 
-        // save new order
-        $order = new Order();
-        $order->store_id = $data['store_id'];
-        $order->user_id = $data['user_id'];
-        $order->code = date('ymdHis') . "-" . $order->store_id . "-" . $order->user_id;
-        $order->save();
+            // save new order
+            $order = new Order();
+            $order->store_id = $data['store_id'];
+            $order->user_id = $data['user_id'];
+            $order->code = date('ymdHis') . "-" . $order->store_id . "-" . $order->user_id;
+            $order->save();
 
-        // save product on order_details
-        $products = $data['products'];
-        for ($i=0; $i < count($products) ; $i++) {
-            $detail = new OrderDetail();
-            $detail->order_id = $order->id;
-            $detail->product_id = $products[$i]['id'];
-            $detail->price = $products[$i]['price'];
-            $detail->quantity = $products[$i]['quantity'];
-            $detail->save();
+            // save product on order_details
+            $products = $data['products'];
+            for ($i=0; $i < count($products) ; $i++) {
+                $detail = new OrderDetail();
+                $detail->order_id = $order->id;
+                $detail->product_id = $products[$i]['id'];
+                $detail->price = $products[$i]['price'];
+                $detail->quantity = $products[$i]['quantity'];
+                $detail->save();
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => "order created",
+                'data' => [
+                    'code' => $order->code,
+                    'order' => new OrderResource($data)
+                ]
+            ], 201);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => $exception,
+                'status' => false
+            ], 500);
         }
 
-        return response()->json([
-            'status' => true,
-            'message' => "order created",
-            'data' => [
-                'code' => $order->code,
-                'order' => new OrderResource($data)
-            ]
-        ], 201);
     }
 }
