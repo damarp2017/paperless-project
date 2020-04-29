cddd2c46463f9db7e8674713557c1755c70c65db
Damar Permadany <damarp2017@gmail.com>
2020-04-29 07:23:16 +0700

    check bug

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -44,7 +44,7 @@ class OrderResource extends JsonResource
             ],
             'order_count' => $count,
             'total_price' => array_sum($total_price),
-            'products' => ProductOnOrderResource::collection($data)
+            'products' => $data
         ];
     }
 }
